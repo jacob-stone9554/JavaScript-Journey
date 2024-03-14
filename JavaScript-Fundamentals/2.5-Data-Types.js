8f0b72673ddc4994f3c71dc25db553595687b20a
Jacob Stone <133823729+jacob-stone9554@users.noreply.github.com>
2024-03-13 20:07:49 -0400

    Committing additional modules (2.5 and 2.6)

---
+++ JavaScript-Fundamentals/2.5-Data-Types.js
@@ -0,0 +1,37 @@
+//in javascript, a value is always of a certain type
+//additionally, javascript is dynamically typed
+//that means you can put any type into a variable
+//for example, one moment it can be a string and the next a number
+
+//the number represents integer AND floating point values.
+let n = 123;
+n = 123.456;
+
+console.log(n);
+
+
+//a string is a combination of characters surrounded by quotation marks
+let s = "hello";
+let embeddedStr = `You can put strings inside strings: ${s} world!`;
+
+console.log(embeddedStr);
+
+
+//boolean is true false
+//you can assign it directly or allow a logical statement to be evaluated
+let bool = true;
+let isGreater = 4 > 1;
+
+console.log(isGreater);
+
+
+
+//if a variable never receives a value, it has the type 'undefined'
+let u;
+
+console.log(u);
+
+
+//you can use the typeof operator to see a variables type
+
+typeof isGreater;
---
+++ JavaScript-Fundamentals/2.6-Alerts-Prompts-Confirms.js
@@ -0,0 +1,16 @@
+//you can allow the user to interact with your browser 
+//  using an alert, a prompt, or a confirm.
+
+//this alert displays a modal with the message hello
+alert("hello");
+
+//this prompt gets the users name as text input
+let result = prompt("Enter your Name", "Enter Name");
+
+alert(result);
+
+
+//example of a confirm
+let isMe = confirm("Are you me?");
+
+alert(isMe);
